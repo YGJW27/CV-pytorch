d1aa9c9bf18e13b188dcc8355a30168caafbce54
杨佳伟 <sheart2v@outlook.com>
2019-06-04 19:13:09 +0800

    v0.1

--- mnist/main.py
+++ mnist/main.py
@@ -2,9 +2,7 @@ import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 import numpy as np
-from torch.autograd import Variable
 from torchvision import datasets, transforms
 
 
@@ -43,7 +41,7 @@ class Net(nn.Module):
         self.fc1.bias.data.fill_(0.0)
         self.FC1_IN = FC1_IN
 
-        # Full Connected Layer 2 
+        # Full Connected Layer 2
         self.fc2 = nn.Linear(FC1_F, FC2_F)
         Fin = FC1_F
         Fout = FC2_F
@@ -54,7 +52,7 @@ class Net(nn.Module):
         # Max Pooling
         self.pool = nn.MaxPool2d(2, 2)
 
-    def forward(self, x, prob):
+    def forward(self, x, prob=0):
         # Convolutional Layer 1
         x = self.conv1(x)
         x = F.relu(x)
@@ -73,8 +71,104 @@ class Net(nn.Module):
 
         # Full Connected Layer 2
         x = self.fc2(x)
-        
+
         return x
 
 
-        
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+
+        # Forward
+        kwargs = {'prob': args.drop}
+        output = model(data, **kwargs)
+        loss = F.cross_entropy(output, target)
+
+        # Backword
+        loss.backward()
+
+        # Update
+        optimizer.step()
+
+        # Evaluate
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    accuracy = correct / len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {:.0f}%\n'.format(
+        test_loss, 100. * accuracy))
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description="Pytorch MNIST")
+    parser.add_argument('-B', '--batchsize', type=int, default=1, metavar='B')
+    parser.add_argument('-E', '--epochs', type=int, default=30, metavar='N')
+    parser.add_argument('-L', '--lr', type=float, default=0.01, metavar='LR')
+    parser.add_argument('-M', '--momentum', type=float, default=0.9, metavar='M')
+    parser.add_argument('-D', '--drop', type=float, default=0.5, metavar='D')
+    parser.add_argument('-S', '--seed', type=int, default=1, metavar='S')
+    parser.add_argument('-C', '--cuda', action='store_true', default=False)
+    args = parser.parse_args()
+
+    use_cuda = args.cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    torch.manual_seed(args.seed)
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batchsize, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=1, shuffle=True, **kwargs)
+
+    # network parameters
+    IN_W = 28
+    IN_H = 28
+    IN_C = 1
+    CL1_F = 32
+    CL1_K = 5
+    CL2_F = 64
+    CL2_K = 5
+    FC1_F = 512
+    FC2_F = 10
+    net_parameters = [IN_W, IN_H, IN_C, CL1_F, CL1_K, CL2_F, CL2_K, FC1_F, FC2_F]
+    model = Net(net_parameters).to(device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader)
+
+    torch.save(model.state_dict(), "mnist_cnn.pt")
+
+
+if __name__ == "__main__":
+    main()
