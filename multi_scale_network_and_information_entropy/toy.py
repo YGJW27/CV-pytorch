6d93f85507eededb4e9568048a435d2a095f5269
Jiawei Yang <sheart2v@outlook.com>
2020-05-09 09:59:27 +0800

    add grid search

--- multi_scale_network_and_information_entropy/toy.py
+++ multi_scale_network_and_information_entropy/toy.py
@@ -1,9 +1,12 @@
+import time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import KFold
-from sklearn.svm import SVC, LinearSVC
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
@@ -60,36 +63,28 @@ def main_2():
     NODE_PATH = "D:/code/DTI_data/network_distance/AAL_90_num.node"
     GRAPH_PATH = "D:/code/DTI_data/network_distance/grouplevel.edge"
     output_path = "D:/code/mutual_information_toy_output/"
-    shape = 32
+    shape = 10
     sample_num = 100
+    np.random.seed(123)
     random_seed = 123456
-    x, y, _ = nxnetwork_generate(shape, sample_num, random_seed)
-
-    node_df = pd.read_csv(NODE_PATH, sep=' ', header=None)
-    region = node_df.iloc[:, 3].to_numpy()
-    node1 = np.where(region==1)[0]
-    node2 = np.where(region==2)[0]
-    node3 = np.where(region==3)[0]
-    node4 = np.where(region==4)[0]
-    node5 = np.where(region==5)[0]
     
-    ggraph = pd.read_csv(GRAPH_PATH, sep='\t', header=None).to_numpy()
-    g1 = np.matrix(ggraph[node1, :][:, node1])
-    g2 = np.matrix(ggraph[node2, :][:, node2])
-    g3 = np.matrix(ggraph[node3, :][:, node3])
-    g4 = np.matrix(ggraph[node4, :][:, node4])
-    g5 = np.matrix(ggraph[node5, :][:, node5])
+    # MI learning parameter
+    k = 10
+    sparse_rate = 1.0
 
-    g = g3[:shape, :][:, :shape]
+    x, y, _ = nxnetwork_generate(shape, sample_num, random_seed)
+    g = graph_mine(x, y, sparse_rate)
+
     starttime = time.time()
 
     # PSO parameters
     part_num = 20
-    iter_num = 300
+    iter_num = 1000
     omega_max = 0.9
     omega_min = 0.4
     c1 = 2
     c2 = 2
+
 
     # 10-fold validation
     cv = 10
@@ -101,25 +96,56 @@ def main_2():
         y_train = y[train_idx]
         y_test = y[test_idx]
 
-        model = MI_learning(x_train, y_train, g, 4)
+        fs_num = 10
+        pca_num = 10
+
+        model = MI_learning(x_train, y_train, g, k)
         b_list, MI_list = model.learning(part_num, iter_num, omega_max, omega_min, c1, c2)
-        # f1_train = np.matmul(x_train, b1)
-        # f1_test = np.matmul(x_test, b1)
-        # df = pd.DataFrame(np.array([part_num, iter_num, b1, MI_array1]).reshape(1, -1), columns=['part_num', 'iter_num', 'b', 'MI'])
-        # plt.plot(MI_array1)
 
-        # df.to_csv(output_path + \
-        #     'w1_dim_{:d}_part_num_{:d}.csv'.format(shape, part_num),
-        #     index=False
-        #     )
-        # plt.savefig(output_path + \
-        #     'w1_dim_{:d}_part_num_{:d}.png'.format(shape, part_num)
-        #     )
+        b_df = pd.DataFrame(b_list)
+        MI_df = pd.DataFrame(MI_list)
+        b_df.to_csv(output_path + 'shape_{:d}_k_{:d}_sparserate_{:.1f}_b_list_cv_{:d}.csv'.format(shape, k, sparse_rate, idx), header=False,
+            index=False
+            )
+        MI_df.to_csv(output_path + 'shape_{:d}_k_{:d}_sparserate_{:.1f}_MI_list_cv_{:d}.csv'.format(shape, k, sparse_rate, idx), header=False,
+            index=False
+            )
+
+        f1_train = []
+        f1_test = []
+        for b in b_list:
+            f_train = np.matmul(x_train, b)
+            f1_train.append(f_train)
+            f_test = np.matmul(x_test, b)
+            f1_test.append(f_test)
+        f1_train = np.array(f1_train)
+        f1_train = np.transpose(f1_train, (1, 0, 2))
+        f1_train = np.reshape(f1_train, (f1_train.shape[0], -1))
+        f1_test = np.array(f1_test)
+        f1_test = np.transpose(f1_test, (1, 0, 2))
+        f1_test = np.reshape(f1_test, (f1_test.shape[0], -1))
+
+        # fisher scoring
+        class0 = f1_train[y_train == 0]
+        class1 = f1_train[y_train == 1]
+        Mu0 = np.mean(class0, axis=0)
+        Mu1 = np.mean(class1, axis=0)
+        Mu = np.mean(f1_train, axis=0)
+        Sigma0 = np.var(class0, axis=0)
+        Sigma1 = np.var(class1, axis=0)
+        n0 = class0.shape[0]
+        n1 = class1.shape[0]
+        fisher_score = (n0 * (Mu0 - Mu)**2 + n1 * (Mu1 - Mu)**2) / (n0 * Sigma0 + n1 * Sigma1)
+        sort_idx = np.argsort(fisher_score)[::-1]
+        sort_idx = sort_idx[:fs_num]
+        
+        f1_train = f1_train[:, sort_idx]
+        f1_test = f1_test[:, sort_idx]
 
 
-        # matrix to array
-        f1_train = x_train.reshape(x_train.shape[0], -1)
-        f1_test = x_test.reshape(x_test.shape[0], -1)
+        # # matrix to array
+        # f1_train = x_train.reshape(x_train.shape[0], -1)
+        # f1_test = x_test.reshape(x_test.shape[0], -1)
 
         # Norm
         scaler = StandardScaler()
@@ -127,15 +153,52 @@ def main_2():
         f1scale_train = scaler.transform(f1_train)
         f1scale_test = scaler.transform(f1_test)
 
-        # PCA
-        # pca = PCA(n_components=shape)
+        # # PCA
+        # pca = PCA(n_components=pca_num)
         # pca.fit(f1scale_train)
         # f1scale_train = pca.transform(f1scale_train)
         # f1scale_test = pca.transform(f1scale_test)
 
+        # # Set the parameters by cross-validation
+        # tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4, 1e-5],
+        #                     'C': [1, 10, 100, 1000]},
+        #                     {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+
+        # scores = ['precision', 'recall']
+
+        # for score in scores:
+        #     print("# Tuning hyper-parameters for %s" % score)
+        #     print()
+
+        #     clf = GridSearchCV(
+        #         SVC(), tuned_parameters, scoring='%s_macro' % score
+        #     )
+        #     clf.fit(f1scale_train, y_train)
+
+        #     print("Best parameters set found on development set:")
+        #     print()
+        #     print(clf.best_params_)
+        #     print()
+        #     print("Grid scores on development set:")
+        #     print()
+        #     means = clf.cv_results_['mean_test_score']
+        #     stds = clf.cv_results_['std_test_score']
+        #     for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        #         print("%0.3f (+/-%0.03f) for %r"
+        #             % (mean, std * 2, params))
+        #     print()
+
+        #     print("Detailed classification report:")
+        #     print()
+        #     print("The model is trained on the full development set.")
+        #     print("The scores are computed on the full evaluation set.")
+        #     print()
+        #     y_true, y_pred = y_test, clf.predict(f1scale_test)
+        #     print(classification_report(y_true, y_pred))
+        #     print()
 
         # SVC
-        svc = SVC(kernel='rbf', random_state=1, gamma=0.001, C=10)
+        svc = SVC(kernel='rbf', random_state=1, gamma=0.001, C=1000)
         model = svc.fit(f1scale_train, y_train)
 
         predict_train = model.predict(f1scale_train)
@@ -144,13 +207,18 @@ def main_2():
 
         predict = model.predict(f1scale_test)
         correct = np.sum(predict == y_test)
+        print()
+        print(classification_report(y_test, predict))
         accuracy = correct / test_idx.size
         print("cv: {}/{}, acc.: {:.1f}/{:.1f}\n".format(idx, cv, accuracy_train*100, accuracy*100))
         acc_sum += accuracy
         print("total acc.: {:.1f}\n".format(acc_sum / cv * 100))
+        print()
 
     endtime = time.time()
     runtime = endtime - starttime
+    print('total time:', runtime, '\n')
+    print()
 
 
 if __name__ == "__main__":
